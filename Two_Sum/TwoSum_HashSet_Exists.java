33d6642e72d52a2feec2384d77fe6d11127d5dd7
Harshini <23a81a0541@sves.org.in>
2026-06-10 14:30:00 +0530

    Hash set

---
+++ Two_Sum/TwoSum_HashSet_Exists.java
@@ -0,0 +1,24 @@
+package Two_Sum;
+
+import java.util.*;
+
+public class TwoSum_HashSet_Exists {
+    public static void main(String[] args) {
+        // Hardcoded Input
+        int[] arr = {1, 4, 7, 2, 5, 8};
+        int target = 10;
+
+        HashSet<Integer> set = new HashSet<>();
+        boolean found = false;
+
+        for (int num : arr) {
+            if (set.contains(target - num)) {
+                found = true;
+                break;
+            }
+            set.add(num);
+        }
+
+        System.out.println(found ? "Pair exists" : "No pair");
+    }
+}
