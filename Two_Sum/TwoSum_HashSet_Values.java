1f0cee54afafe23366d216f19c23aa8c47866976
Harshini <23a81a0541@sves.org.in>
2026-06-12 14:30:00 +0530

    Hash set

---
+++ Two_Sum/TwoSum_HashSet_Values.java
@@ -0,0 +1,23 @@
+package Two_Sum;
+import java.util.*;
+
+public class TwoSum_HashSet_Values {
+    public static void main(String[] args) {
+    
+        int[] arr = {1, 4, 7, 2, 5, 8};
+        int target = 10;
+
+        HashSet<Integer> set = new HashSet<>();
+        boolean found = false;
+
+        for (int num : arr) {
+            if (set.contains(target - num)) {
+                System.out.println("Pair: " + (target - num) + ", " + num);
+                found = true;
+            }
+            set.add(num);
+        }
+
+        if (!found) System.out.println("No pair found");
+    }
+}
