f7162ee9008a0729b24f7dcdf6daabbb3e6b1d61
Harshini <23a81a0541@sves.org.in>
2026-06-14 14:30:00 +0530

    Hash set

---
+++ Two_Sum/TwoSum_TwoPointer_Exists_Sort.java
@@ -0,0 +1,30 @@
+package Two_Sum;
+
+import java.util.*;
+
+public class TwoSum_TwoPointer_Exists_Sort {
+    public static void main(String[] args) {
+        
+        int[] arr = {1, 4, 7, 2, 5, 8};
+        int target = 10;
+        int n = arr.length;
+        Arrays.sort(arr);
+
+        boolean found = false;
+        int left = 0, right = n - 1;
+
+        while (left < right) {
+            int sum = arr[left] + arr[right];
+            if (sum == target) {
+                found = true;
+                break;
+            } else if (sum < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        System.out.println(found ? "Pair exists" : "No pair");
+    }
+}
