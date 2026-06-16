d0ce7bf38cbd747e1e628f0ca32aec59464ec25a
Harshini <23a81a0541@sves.org.in>
2026-06-16 14:30:00 +0530

    Hash set

---
+++ Two_Sum/TwoSum_TwoPointer_Indices_Break.java
@@ -0,0 +1,31 @@
+package Two_Sum;
+import java.util.*;
+
+public class TwoSum_TwoPointer_Indices_Break {
+    public static void main(String[] args) {
+        
+        int[] arr = {1, 4, 7, 2, 5, 8};
+        int target = 10;
+        int n = arr.length;
+
+        Arrays.sort(arr);
+
+        boolean found = false;
+        int left = 0, right = n - 1;
+
+        while (left < right) {
+            int sum = arr[left] + arr[right];
+            if (sum == target) {
+                System.out.println("Indices: " + left + ", " + right);
+                found = true;
+                break; 
+            } else if (sum < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        if (!found) System.out.println("No pair found");
+    }
+}
